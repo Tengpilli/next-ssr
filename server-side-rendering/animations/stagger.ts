12397f8fc4a9fddbac1f27ae4d21b2785fd268e6
Senior <wrfcore816@gmail.com>
2022-11-24 20:33:07 -0600

    ad

--- server-side-rendering/animations/stagger.ts
+++
@@ -1,9 +0,0 @@
-import { Variant } from "framer-motion";
-
-export default (staggerChildren = 0.3): Variant => ({
-  animate: {
-    transition: {
-      staggerChildren,
-    },
-  },
-});
--- static-site-generation/animations/stagger.ts
+++
@@ -1,9 +0,0 @@
-import { Variants } from "framer-motion";
-
-export default (staggerChildren = 0.3): Variants => ({
-  animate: {
-    transition: {
-      staggerChildren,
-    },
-  },
-});
